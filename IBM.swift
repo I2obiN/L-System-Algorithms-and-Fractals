5b97508241947a10ccb7e31626f1ec4e8edc7908
Thomas Hood <thomashood10@yahoo.com>
2016-02-02 08:50:06 +0000

    Add scaling function

--- IBM.swift
+++ IBM.swift
@@ -4,15 +4,18 @@
     /**
     * Created by I2obiN on 29/01/16.
     */
-
-    var heavy1: String = " ▒▒▒ ▒▒▒  ▒▒ ▒▒";
-    var light2: String = "  ░  ░  ░ ░ ░ ░";
-    var light3: String = "  ░  ░░░  ░ ░ ░";
-    var light4: String = "  ░  ░  ░ ░ ░ ░";
-    var heavy5: String = " ░░░ ░░░  ░   ░";
     
-    var array: [String] = [heavy1, light2, light3, light4, heavy5];
+    var bar1: String = " ━━━━━━━━━━━━━━━━━━━"
+    var heavy1: String = " ▄▄▄  ▄▄▄▄   ▄▄▄ ▄▄▄";
+    var light2: String = "  ▄    ▄  ▄   ▄ ▄ ▄";
+    var light3: String = "  ▄    ▄ ▄    ▄   ▄";
+    var light4: String = "  ▄    ▄  ▄   ▄   ▄";
+    var heavy5: String = " ▄▄▄  ▄▄▄▄   ▄▄   ▄▄";
+    var bar2: String = " ━━━━━━━━━━━━━━━━━━━"
+    
+    var array: [String] 
+    = [bar1, heavy1, light2, light3, light4, heavy5, " ", bar2];
     
     for string in array {
-        print(string+" "+string+" "+string+" "+string+" "+string);
+        print(string);
     }
